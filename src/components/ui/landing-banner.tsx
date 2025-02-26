7f808288e5a1ee4f66144ff3f17f564041edf22d
Ben Phillips <benjamin.phillips@winning.com.au>
2025-02-26 17:21:31 +1100

    added chakra ui

---
+++ src/components/ui/landing-banner.tsx
@@ -0,0 +1,10 @@
+// export default function LandingBanner() {
+//     return (
+//       <div className="w-9/12 mx-auto relative">
+//         <img src="/images/ninth-wave.jpg" alt="Landing Banner" />
+//         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+//           <h1 className="text-white text-4xl font-bold">YOUNG MEN OF GOD</h1>
+//         </div>
+//       </div>
+//     );
+//   }
