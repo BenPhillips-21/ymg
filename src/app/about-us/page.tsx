d014977069e5f798709efe962f3df152eb43eaeb
Ben Phillips <benjamin.phillips@winning.com.au>
2025-04-14 16:50:04 +1000

    changed text sizing

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -7,28 +7,28 @@ export default function Page() {
     <Main>
       <div className="p-4 flex flex-col md:flex-row">
         <div className="md:w-1/2 md:pr-4">
-          <h1 className={`text-2xl font-bold mb-4 ${montserrat.className}`}>
+          <h1 className={`text-xl lg:text-2xl font-bold mb-4 ${montserrat.className}`}>
             YMG Origins
           </h1>
-          <p className={`mb-5 ${montserrat.className}`}>
+          <p className={`text-xs lg:text-l mb-5 ${montserrat.className}`}>
             When Fr Ken Barker MGL was in Assisi in the Jubilee Year 2000 praying
-            in the small church where St Francis and his brothers spent much of
+            in the xsall church where St Francis and his brothers spent much of
             their time, he sensed the Lord wanted a movement of young men in
             Australia, which would spread also to other countries.
           </p>
-          <p className={`mb-4 ${montserrat.className}`}>
+          <p className={`text-xs lg:text-l mb-4 ${montserrat.className}`}>
             He sensed the Lord wanted young men to rise up in the Church today
             with a deep faith, purity of heart, and strength of character, to
             become leaders for the future. The Lord wanted to help young men to
             change; to know they can be made new.
           </p>
-          <p className={`mb-4 ${montserrat.className}`}>
+          <p className={`text-xs lg:text-l mb-4 ${montserrat.className}`}>
             Fr Ken shared this vision with some young men in Canberra who had
             already begun to gather with other young men to support one another
             around, sport, teachings, and personal sharing. They shared the
             vision together and a movement was born.
           </p>
-          <p className={`mb-4 ${montserrat.className}`}>
+          <p className={`text-xs lg:text-l mb-4 ${montserrat.className}`}>
             During Covid, and due to other influences, the movement lost
             momentum. We are now in a moment of renaissance with renewed vision
             and vigour to fulfill the Lord’s dream for men. This renewal has
--- src/components/ui/ourMission.tsx
+++ src/components/ui/ourMission.tsx
@@ -3,8 +3,8 @@ import { montserrat } from "./fonts";
 export default function OurMission() {
     return (
       <section className="flex flex-col items-center justify-center max-w-full px-4 py-8 mx-auto space-y-4">
-        <h1 className={`${montserrat.className} text-[#1c272c] text-4xl lg:text-6xl font-bold text-center underline`}>Our Mission</h1> 
-        <h1 className={`${montserrat.className} text-2xl lg:text-4xl text-[#1c272c] text-center leading-relaxed`}>  
+        <h1 className={`${montserrat.className} text-[#1c272c] text-3xl lg:text-4xl font-bold text-center underline`}>Our Mission</h1> 
+        <h1 className={`${montserrat.className} text-1xl lg:text-2xl text-[#1c272c] text-center leading-relaxed`}>  
           To disciple and equip young men with the truth of the gospel for personal growth in Christ, and in brotherhood and evangelisation.
         </h1>
       </section>
