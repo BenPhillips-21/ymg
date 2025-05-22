e22d38a8cc9afdf43ceae9f35f7f10df2fa0c9e8
Ben Phillips <benjamin.phillips@winning.com.au>
2025-05-22 21:34:46 +1000

    updated migrations

--- migrations/0000_flawless_gamora.sql
+++ migrations/0000_flawless_gamora.sql
@@ -1,18 +1,13 @@
-CREATE TABLE `posts` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`title` text NOT NULL,
-	`content` text NOT NULL,
-	`user_id` integer NOT NULL,
-	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
-	`updated_at` integer,
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
-);
 --> statement-breakpoint
 CREATE TABLE `users` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
 	`age` integer NOT NULL,
-	`email` text NOT NULL
+	`email` text NOT NULL,
+	`phone` text NOT NULL,
+	`location` text NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_phone_unique` ON `users` (`phone`);
