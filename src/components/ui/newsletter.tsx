cbff09820db0d258c3dcdd2f92beaa6eabac16a2
Ben Phillips <benjamin.phillips@winning.com.au>
2025-08-15 11:06:13 +1000

    removed useRef

--- src/components/ui/newsletter.tsx
+++ src/components/ui/newsletter.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
